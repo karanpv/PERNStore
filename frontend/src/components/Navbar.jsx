32185e6601b99201e8124de7f0d61513d85037fc
karanpv <149856183+karanpv@users.noreply.github.com>
2025-05-16 11:07:08 +0530

    navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const { pathname } = useResolvedPath();
   const isHomePage = pathname === '/';
 
-  const {products} = useProductStore();
+  const { products } = useProductStore();
 
   return (
     <div className='bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50'>
